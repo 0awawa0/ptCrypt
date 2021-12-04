a6c19bc16c76db0f72ed728377cb27810ab7ff1a
Alexander Pozdniakov <mail.0awawa0@gmail.com>
2021-12-04 14:37:46 +0300

    Implemented probabl primes generation and validation for DSA

--- DSA.py
+++ DSA.py
@@ -1,101 +1,239 @@
 import mathbase
 import random
 import hashlib
+import math
 
 
 class DSA:
 
+	APPROVED_LENGTHS = [
+		(160, 1024),
+		(224, 2048),
+		(256, 2048),
+		(256, 3072)
+	]
+
+	class Params:
+
+		class Primes:
+			def __init__(self, p: int, q: int):
+				self.p = p
+				self.q = q
+			
+			def beautyRepr(self, level: int) -> str:
+				indent = "\t" * level
+				return f"Primes: \n{indent}p: {hex(self.p)}\n{indent}q: {hex(self.q)}"
+
+
+		class ProbablePrimesGenerationResult:
+			class ProbablePrimesVerifyParams:
+				def __init__(self, domainParameterSeed: int, counter: int):
+					self.domainParameterSeed = domainParameterSeed
+					self.counter = counter
+				
+				def beautyRepr(self, level: int) -> str:
+					indent = "\t" * level
+					return f"ProbablePrimesVerifyParams:\n{indent}domainPrameterSeed: {hex(self.domainParameterSeed)}\n{indent}counter: {hex(self.counter)}"
+			
+			def __init__(self, status: bool, primes, verifyParams: ProbablePrimesVerifyParams):
+				self.status = status
+				self.primes = primes
+				self.verifyParams = verifyParams
+
+			def beautyRepr(self, level: int) -> str:
+				primesRepr = self.primes.beautyRepr(level + 1)
+				verifyParamsRepr = self.verifyParams.beautyRepr(level + 1)
+				indent = "\t" * level
+				return f"ProbablePrimesGenerationResult: \n{indent}status: {self.status}\n{indent}{primesRepr}\n{indent}{verifyParamsRepr}"
+			
+		def __init__(self, primes: Primes, g: int):
+			self.primes = primes
+			self.g = g
+
+		def beautyRepr(self, level: int) -> str:
+			primesRepr = self.primes.beautyRepr(level + 1)
+			indent = "\t" * level
+			return f"DSA Params: \n{indent}{primesRepr}\n{indent}g: {self.g}"
+
+	class PublicKey:
+
+		def __init__(self, params, y: int):
+			self.params = params
+			self.y = y
+
+	class PrivateKey:
+
+		def __init__(self, params, x: int):
+			self.params = params
+			self.x = x
+	
 	class Signature:
 
-		def __init__(self, params, r, s):
+		def __init__(self, params, r: int, s: int):
 			self.params = params
 			self.r = r
 			self.s = s
 
-	class Params:
+	def generateProbablePrimes(N: int, L: int, seedLength: int, hashFunction=hashlib.sha256):
 
-		def __init__(self, biggerPrime, smallerPrime, g):
-			self.biggerPrime = biggerPrime
-			self.smallerPrime = smallerPrime
-			self.g = g
+		if (N, L) not in DSA.APPROVED_LENGTHS: return DSA.Params.ProbablePrimesGenerationResult(False, None, None)
+		if seedLength < N: DSA.Params.ProbablePrimesGenerationResult(False, None, None)
 
-	class PublicKey:
+		# Length of hash funciton output in bits
+		outlen = hashFunction().digest_size * 8
+		if outlen < N: return DSA.Params.ProbablePrimesGenerationResult(False, None, None)
 
-		def __init__(self, params, key):
-			self.params = params
-			self.key = key
+		if L % outlen == 0:
+			n = L // outlen - 1
+		else:
+			n = L // outlen
+		
+		b = L - 1 - (n * outlen)
 
-	class PrivateKey:
+		twoPowNMin1 = pow(2, N - 1)
+		twoPowSeedLength = pow(2, seedLength)
+		twoPowOutLength = pow(2, outlen)
+		twoPowLMin1 = pow(2, L - 1)
+		twoPowB = pow(2, b)
 
-		def __init__(self, params, key):
-			self.params = params
-			self.key = key
-
-	def __init__(self, keyLength, primeLength):
-		self.primeLength = primeLength
-		self.keyLength = keyLength
-		self.generateParams()
-		self.generateKey()
-
-	def generateParams(smallerLength, biggerLength):
-	
-		q = 0
-		p = 0
-		tries = 0
 		while 1:
-			tries += 1
-			q = mathbase.getPrime(smallerLength)
-			k = biggerLength - smallerLength
-			p = (q << k) + 1
-			if mathbase.isPrime(p, 10):
-				print(p.bit_length())
-				print(q.bit_length())
-				print(f'Found params in {tries} tries')
-				break
-		i = 0
-		k = mathbase.SMALL_PRIMES[i]
-		g = pow(k, (p - 1) // q, p)
-		while g == 1:
-			i += 1
-			if i >= len(mathbase.SMALL_PRIMES):
-				return None
 
-			k = mathbase.SMALL_PRIMES[i]
-			g = pow(k, (p - 1) // q, p)
+			while 1:
+				domainParameterSeed = random.getrandbits(seedLength)
+				U = mathbase.bytesToInt(hashFunction(mathbase.intToBytes(domainParameterSeed)).digest()) % twoPowNMin1
+				q = twoPowNMin1 + U + 1 - (U % 2)
+				if mathbase.isPrime(q, 10):
+					break
 
-		return DSA.Params(p, q, g)
+			twoTimesQ = 2 * q
 
+			offset = 1
+			for counter in range(0, 4 * L - 1):
+				W = 0
+				for j in range(0, n):
+					hashPayload = mathbase.intToBytes((domainParameterSeed + offset + j) % twoPowSeedLength)
+					v = mathbase.bytesToInt(hashFunction(hashPayload).digest())
+					W += v * pow(twoPowOutLength, j)
+		
+				hashPayload = mathbase.intToBytes((domainParameterSeed + offset + n) % twoPowSeedLength)
+				v = int(mathbase.bytesToInt(hashFunction(hashPayload).digest()) % twoPowB)
+				W += v * pow(twoPowOutLength, n)
 
-	def generateKeys(params):
-		x = random.randint(1, params.smallerPrime - 1)
-		y = pow(params.g, x, params.biggerPrime)
+				X = W + twoPowLMin1
+				c = X % twoTimesQ
+				p = X - (c - 1)
+				if p >= twoPowLMin1:
+					if mathbase.isPrime(p, 10):
+						primes = DSA.Params.Primes(p, q)
+						verifyParams = DSA.Params.ProbablePrimesGenerationResult.ProbablePrimesVerifyParams(domainParameterSeed, counter)
+						return DSA.Params.ProbablePrimesGenerationResult(True, primes, verifyParams)
+				offset = offset + n + 1
+
+		return DSA.Params.ProbablePrimesGenerationResult(False, None, None)
+
+	def verifyProbablePrimesGenerationResult(result, hashFunction=hashlib.sha256) -> bool:
+		p = result.primes.p
+		q = result.primes.q
+		domainParameterSeed = result.verifyParams.domainParameterSeed
+		counter = result.verifyParams.counter
+
+		N = q.bit_length()
+		L = p.bit_length()
+		if (N, L) not in DSA.APPROVED_LENGTHS: 
+			print(1)
+			return False
+
+		if counter > (4 * L - 1): 
+			print(2)
+			return False
+
+		seedLength = domainParameterSeed.bit_length()
+		if seedLength < N: 
+			print(3)
+			return False
+
+		twoPowNMin1 = pow(2, N - 1)
+		hashPayload = mathbase.intToBytes(domainParameterSeed)
+		U = mathbase.bytesToInt(hashFunction(hashPayload).digest()) % twoPowNMin1
+		computedQ = pow(2, N - 1) + U + 1 - (U % 2)
+		if computedQ != q or (not mathbase.isPrime(computedQ, 10)):
+			print(4)
+			return False
+		
+		outlen = hashFunction().digest_size * 8
+		if L % outlen == 0:
+			n = L // outlen - 1
+		else:
+			n = L // outlen
+		
+		b = L - 1 - (n * outlen)
+		twoPowSeedLength = pow(2, seedLength)
+		twoPowOutLength = pow(2, outlen)
+		twoPowLMin1 = pow(2, L - 1)
+		twoPowB = pow(2, b)
+		offset = 1
+
+		twoTimesQ = 2 * q
+		for i in range(counter + 1):
+			W = 0
+			for j in range(0, n):
+				hashPayload = mathbase.intToBytes((domainParameterSeed + offset + j) % twoPowSeedLength)
+				v = mathbase.bytesToInt(hashFunction(hashPayload).digest())
+				W += v * pow(twoPowOutLength, j)
+		
+			hashPayload = mathbase.intToBytes((domainParameterSeed + offset + n) % twoPowSeedLength)
+			v = int(mathbase.bytesToInt(hashFunction(hashPayload).digest()) % twoPowB)
+			W += v * pow(twoPowOutLength, n)
+
+			X = W + twoPowLMin1
+			c = X % twoTimesQ
+			computed_p = X - (c - 1)
+			if computed_p < twoPowLMin1:
+				offset = offset + n + 1
+				continue
+				
+			if mathbase.isPrime(computed_p, 10):
+				if i == counter and computed_p == p:
+					return True
+				else:
+					print(5)
+					return False
+			
+			offset = offset + n + 1
+
+		print(6)
+		return False
+
+	def generateKeys(params: Params):
+		x = random.randint(1, params.q - 1)
+		y = pow(params.g, x, params.p)
 		publicKey = DSA.PublicKey(params, y)
 		privateKey = DSA.PrivateKey(params, x)
 		return (publicKey, privateKey)
 
 
-	def sign(m, key):
+	def sign(m: int, key: PrivateKey):
 		r = 0
 		s = 0
 		while r == 0 or s == 0:
-			k = random.randint(1, key.params.smallerPrime - 1)
-			r = pow(key.params.g, k, key.params.biggerPrime) % key.params.smallerPrime
-			s = pow(k, -1, key.params.smallerPrime) * (m + key.key * r) % key.params.smallerPrime
+			k = random.randint(1, key.params.q - 1)
+			r = pow(key.params.g, k, key.params.p) % key.params.q
+			s = pow(k, -1, key.params.q) * (m + key.x * r) % key.params.q
 		return DSA.Signature(params, r, s)
 
 
-	def verify(m, signature, key):
-		w = pow(signature.s, -1, key.params.smallerPrime)
-		u1 = w * m % key.params.smallerPrime
-		u2 = w * signature.r % key.params.smallerPrime
-		v = (pow(key.params.g, u1, key.params.biggerPrime) * pow(key.key, u2, key.params.biggerPrime) % key.params.biggerPrime) % key.params.smallerPrime
+	def verify(m: int, signature: Signature, key: PublicKey):
+		w = pow(signature.s, -1, key.params.q)
+		u1 = w * m % key.params.q
+		u2 = w * signature.r % key.params.q
+		v = (pow(key.params.g, u1, key.params.p) * pow(key.y, u2, key.params.p) % key.params.p) % key.params.q
 		return v == signature.r
 
 
-upperBound = 2 ** 99 - 1
-m = random.randint(1, upperBound)
-params = DSA.generateParams(512, 1024)
-(public, private) = DSA.generateKeys(params)
-signature = DSA.sign(m, private)
-assert DSA.verify(m, signature, public)
-print(f"Check done")
+for i in range(10):
+	for lengths in DSA.APPROVED_LENGTHS:
+		N, L = lengths
+		params = DSA.generateProbablePrimes(N, L, L)
+		print(params.beautyRepr(1))
+		assert DSA.verifyProbablePrimesGenerationResult(params)
+		print("Parameters verified")
