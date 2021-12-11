332b79e53c706446f926a224ee7952f92b74aaf9
Alexander Pozdniakov <mail.0awawa0@gmail.com>
2021-12-11 23:03:13 +0300

    Edited setup.py and setup.cfg

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'aCrypt',
+    packages = ['aCrypt'],
+    version = '0.0.1',
+    description = 'Cryptographic and cryptography related algorithms implemented with pure Python',
+    author = 'awawa',
+    author_email = 'mail.0awawa0@gmail.com',
+    url = 'https://github.com/0awawa0/aCrypt',
+    download_url = 'https://github.com/0awawa0/aCrypt/archive/refs/tags/v0.0.1.tar.gz',
+    keywords = ['Cryptography'],
+    classifiers = [],
+)
