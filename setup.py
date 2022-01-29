7bd18121e0ca9a83c27e3cf0f6b41c3cb2dd6d35
Alexander Pozdniakov <mail.0awawa0@gmail.com>
2022-01-29 14:31:02 +0300

    Releasing version 0.0.5

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import find_packages
 setup(
     name = 'ptCrypt',
     packages = find_packages(),
-    version = '0.0.4',
+    version = '0.0.5',
     description = 'Cryptographic and cryptography related algorithms implemented with pure Python',
     author = 'awawa',
     author_email = 'mail.0awawa0@gmail.com',
     url = 'https://github.com/0awawa0/ptCrypt',
-    download_url = 'https://github.com/0awawa0/ptCrypt/archive/refs/tags/0.0.4.tar.gz',
+    download_url = 'https://github.com/0awawa0/ptCrypt/archive/refs/tags/0.0.5.tar.gz',
     keywords = ['Cryptography'],
     classifiers = [],
 )
